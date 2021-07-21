3b09d8fe74eaf041caaaa813316ced80e7cc2e8c
cibikumar <cibikumarv@gmail.com>
2021-07-21 20:16:33 +0530

    Added provider library for state management.

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   static const routeName = "/product-detail";
 
   @override
   Widget build(BuildContext context) {
     final productID = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productID);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Title"),
+        title: Text(loadedProduct.title),
       ),
     );
   }
