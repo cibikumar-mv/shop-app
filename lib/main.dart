8ee9b395a782a6459aeb4e2b3549f182bee28ba5
cibikumar <cibikumarv@gmail.com>
2021-07-29 08:24:48 +0530

    Cleaned the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/cart.dart';
-import 'package:shop_app/providers/orders.dart';
-import 'package:shop_app/providers/products_provider.dart';
-import 'package:shop_app/screens/cart_screen.dart';
-import 'package:shop_app/screens/orders_screen.dart';
-import 'package:shop_app/screens/product_detail_screen.dart';
-import 'package:shop_app/screens/products_overview_screen.dart';
+import '/providers/cart.dart';
+import '/providers/orders.dart';
+import '/providers/products_provider.dart';
+import '/screens/cart_screen.dart';
+import '/screens/orders_screen.dart';
+import '/screens/product_detail_screen.dart';
+import '/screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,9 +1,9 @@
 import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:shop_app/providers/orders.dart' as ord;
+import '/providers/orders.dart' as ord;
 
 class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
